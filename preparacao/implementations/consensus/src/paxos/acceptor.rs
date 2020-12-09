159e41b2d4c1aabcb3d59ccd15c64bd86a482d1b
Miguel Alves <42934491+mce-alves@users.noreply.github.com>
2020-12-09 18:45:36 +0000

    Refactored acceptor by adding wrapper functions to handle pre and post conditions of main protocol functions

--- preparacao/implementations/consensus/src/paxos/acceptor.rs
+++ preparacao/implementations/consensus/src/paxos/acceptor.rs
@@ -32,40 +32,59 @@ impl Acceptor {
         }
     }
 
+    // post condition for snd_promise
+    fn post_snd_promise(&mut self) {
+        match self.state {
+            AcceptorState::IDLE => {
+                // update state to promised if currently in IDLE state
+                self.state = AcceptorState::PROMISED;
+            },
+            _ => {}
+        }
+    }
+
     // send PROMISE message to target
     pub fn snd_promise(&mut self, target:mpsc::Sender<Message>, target_pid:i32) -> () {
-        // can send promise in any state, as long as max_id > -1 (has received some prepare)
-        if self.max_id > -1.0 {
-            println!("Acceptor {} sending promise with id={} to Proposer {}.", self.pid, self.max_id, target_pid);
-            target.send(self.create_promise_msg()).unwrap();
-            match self.state {
-                AcceptorState::IDLE => {
-                    // update state to promised if currently in IDLE state
-                    self.state = AcceptorState::PROMISED;
-                },
-                _ => {}
+        println!("Acceptor {} sending promise with id={} to Proposer {}.", self.pid, self.max_id, target_pid);
+        target.send(self.create_promise_msg()).unwrap();
+        self.post_snd_promise();
+    }
+
+
+    // pre condition for snd_accept
+    fn pre_snd_accept(&self) -> bool {
+        match self.state {
+            AcceptorState::ACCEPTED => {
+                println!("Acceptor {} has already accepted a proposal.", self.pid);
+                false
+            },
+            AcceptorState::PROMISED => {
+                true
+            },
+            AcceptorState::IDLE => {
+                println!("Acceptor {} cannot accept a proposal while in IDLE state.", self.pid);
+                false
             }
         }
-        else {
-            println!("Acceptor {} cannot send promise for max_id {}.", self.pid, self.max_id);
-        }
+    }
+
+
+    // post condition for snd_accept
+    fn post_snd_accept(&mut self) {
+        self.state = AcceptorState::ACCEPTED; // update state to ACCEPTED
     }
 
     // send ACCEPTED message to target
     pub fn snd_accept(&mut self, target:mpsc::Sender<Message>, target_pid:i32) -> () {
-        match self.state {
-            AcceptorState::ACCEPTED => println!("Acceptor {} has already accepted a proposal.", self.pid),
-            AcceptorState::PROMISED => {
-                match (self.accepted_id, self.accepted_val) {
-                    (Some(id), Some(val)) => {
-                        println!("Acceptor {} sending accepted with id={}, val={} to Proposer {}.", self.pid, id, val,target_pid);
-                        target.send(self.create_accept_msg(id, val)).unwrap();
-                        self.state = AcceptorState::ACCEPTED; // update state to ACCEPTED
-                    },
-                    _ => println!("Acceptor {} cannot send ACCEPTED because it is missing id or val.", self.pid)
-                }
-            },
-            AcceptorState::IDLE => println!("Acceptor {} cannot accept a proposal while in IDLE state.", self.pid)
+        if self.pre_snd_accept() {
+            match (self.accepted_id, self.accepted_val) {
+                (Some(id), Some(val)) => {
+                    println!("Acceptor {} sending accepted with id={}, val={} to Proposer {}.", self.pid, id, val,target_pid);
+                    target.send(self.create_accept_msg(id, val)).unwrap();
+                    self.post_snd_accept();
+                },
+                _ => println!("Acceptor {} cannot send ACCEPTED because it is missing id or val.", self.pid)
+            }
         }
     }
 
@@ -109,35 +128,64 @@ impl Acceptor {
         }
     }
 
-    // process a received PROPOSE message
-    pub fn rcv_propose(&mut self, msg:Message) -> () {
+
+    // pre condition for rcv_propose
+    fn pre_rcv_propose(&self) -> bool {
         match self.state {
             AcceptorState::PROMISED => {
-                match (msg.msg_type, msg.propose) {
-                    (MessageType::PROPOSE, Some(proposal)) => {
-                        println!("Acceptor {} received proposal from Proposer {} with id={}, val={}.", self.pid, proposal.sender_pid, proposal.id, proposal.value);
-                        // accept the proposal if it contains the highest ID for which we received a prepare
-                        // P1a : an acceptor can accept a proposal with id N if it has not responded to a prepare request having number greater than N
-                        if proposal.id >= self.max_id {
-                            // SAFETY PROPERTY : only a value that has been proposed may be chosen
-                            self.accepted_id = Some(proposal.id);
-                            self.accepted_val = Some(proposal.value);
-                            self.max_id = proposal.id;
-                            self.snd_accept(proposal.sender, proposal.sender_pid); // also updates state to ACCEPTED
-                            // since this updates the state to accepted, we guarantee another safety property
-                            // SAFETY PROPERTY : only a single value is chosen
-                        }
-                        else {
-                            // reject the proposal
-                            self.snd_reject(proposal.id, proposal.sender);
-                        }
-                    },
-                    _ => println!("Acceptor {} received invalid PROPOSE message.", self.pid)
-                }
+                true
             },
-            _ => println!("Acceptor {} cannot process PROPOSE message in IDLE or ACCEPTED state.", self.pid),
+            _ => {
+                println!("Acceptor {} cannot process PROPOSE message in IDLE or ACCEPTED state.", self.pid);
+                false
+            }
         }
     }
+
+    // process a received PROPOSE message
+    pub fn rcv_propose(&mut self, msg:Message) -> () {
+        if self.pre_rcv_propose() {
+            match (msg.msg_type, msg.propose) {
+                (MessageType::PROPOSE, Some(proposal)) => {
+                    println!("Acceptor {} received proposal from Proposer {} with id={}, val={}.", self.pid, proposal.sender_pid, proposal.id, proposal.value);
+                    // accept the proposal if it contains the highest ID for which we received a prepare
+                    // P1a : an acceptor can accept a proposal with id N if it has not responded to a prepare request having number greater than N
+                    if proposal.id >= self.max_id {
+                        self.accept_proposal(proposal);
+                    }
+                    else {
+                        // reject the proposal
+                        self.snd_reject(proposal.id, proposal.sender);
+                    }
+                },
+                _ => println!("Acceptor {} received invalid PROPOSE message.", self.pid)
+            }
+        }
+    }
+
+    // pre condition for accept_proposal
+    fn pre_accept_proposal(&self, proposal_id:f32) -> bool {
+        proposal_id >= self.max_id
+    }
+
+    // post condition for accept_proposal
+    fn post_accept_proposal(&mut self, proposal_id:f32, proposal_value:i32) {
+        self.accepted_id = Some(proposal_id);
+        self.accepted_val = Some(proposal_value);
+        self.max_id = proposal_id;
+    }
+
+    // accepts a proposal
+    fn accept_proposal(&mut self, proposal:Propose) {
+        if self.pre_accept_proposal(proposal.id) {
+            // SAFETY PROPERTY : only a value that has been proposed may be chosen
+            self.post_accept_proposal(proposal.id, proposal.value);
+            self.snd_accept(proposal.sender, proposal.sender_pid); // also updates state to ACCEPTED
+            // since this updates the state to accepted, we guarantee another safety property
+            // SAFETY PROPERTY : only a single value is chosen
+        }
+    }
+
 
     // create promise message
     fn create_promise_msg(&self) -> Message {
